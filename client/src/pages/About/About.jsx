e85662cae03601824e3d5def24cc2fe58d0816c1
Ashley Primrose <116143986+Primrizzle@users.noreply.github.com>
2025-12-22 01:32:06 -0600

    Update About.jsx
    Added hero section

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,3 +1,54 @@
 export default function About() {
   return <div>About page is working</div>;
 }
+import AboutHero from "../../assets/about/AboutHeroImage.jpg";
+
+export default function About() {
+  return (
+    <div className="w-full">
+      {/* Hero Section */}
+      <section
+        className="
+          relative
+          flex
+          items-center
+          justify-center
+          h-[70vh]
+          min-h-[500px]
+          w-full
+          overflow-hidden
+        "
+      >
+        {/* Background Image */}
+        <img
+          src={AboutHero}
+          alt="Awards ceremony"
+          className="
+            absolute
+            inset-0
+            h-full
+            w-full
+            object-cover
+          "
+        />
+
+        {/* Overlay */}
+        <div className="absolute inset-0 bg-(--secondary)/65" />
+
+        {/* Content */}
+        <div className="relative z-10 max-w-4xl px-6 text-center text-white">
+          <h1 className="mb-4 text-4xl font-semibold tracking-tight md:text-5xl lg:text-6xl"
+              style={{ fontFamily: 'Poppins'}}>
+            About Nourish Laredo
+          </h1>
+
+          <p className="text-lg leading-relaxed md:text-xl" style={{ fontFamily: 'Quicksand'}}>
+            Nourish Laredo is a youth-led nonprofit dedicated to addressing food
+            insecurity while empowering young leaders to serve their community
+            with compassion and purpose.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
